9a342c7b94e0f425588d8511679a9af2ba5bfa11
Lozumi Everkine <fantasynico1026@gmail.com>
2023-10-07 17:25:10 +0800

    Initial team

---
+++ src/main/java/com.lozumi.NAMS/Team.java
@@ -0,0 +1,26 @@
+package com.lozumi.NAMS;
+
+public class Team {
+    String teamId;
+    String teamName;
+    String department;
+    Student Creator;
+
+    public String getTeamId() {
+        return teamId;
+    }
+
+    public String getTeamName() {
+        return teamName;
+    }
+
+    public String getDepartment() {
+        return department;
+    }
+
+    public Student getCreator() {
+        return Creator;
+    }
+
+
+}
