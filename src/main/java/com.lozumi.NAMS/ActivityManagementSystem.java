4aeb43fa5f79e3279c39f6c00fc7e26b04205519
Lozumi Everkine <fantasynico1026@gmail.com>
2023-10-07 17:24:58 +0800

    initial system

---
+++ src/main/java/com.lozumi.NAMS/ActivityManagementSystem.java
@@ -0,0 +1,18 @@
+package com.lozumi.NAMS;
+
+public class ActivityManagementSystem {
+    UserSubSystem usSystem = new UserSubSystem();
+    ActivitySubSystem asSystem = new ActivitySubSystem();
+    public void displayUserSubSystem(){
+        System.out.println(usSystem);
+    }
+    public void displayActivitySubSystem(){
+        System.out.println(asSystem);
+    }
+    public UserSubSystem getUserSubSystem(){
+        return usSystem;
+    }
+    public ActivitySubSystem getActivitySubSystem( {
+        return asSystem;
+    }
+}
---
+++ src/main/java/com.lozumi.NAMS/ActivitySubSystem.java
@@ -0,0 +1,26 @@
+package com.lozumi.NAMS;
+import java.util.List;
+import java.util.Date;
+import java.util.ArrayList;
+
+public class ActivitySubSystem {
+    List<Activity> aList = new ArrayList<>();
+    public void addActivity(Activity activity){
+        aList.add(activity);
+    }
+    public void removeActivityById(String id){
+
+    }
+    public Activity getActivityById(String id){
+
+    }
+    public Activity getActivityByDate(Date date){
+
+    }
+    public List<Activity> getActivityList(){
+
+    }
+    public int checkActivityNumOfTeam(String teamId){
+
+    }
+}
