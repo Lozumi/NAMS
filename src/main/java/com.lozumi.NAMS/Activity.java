ec2503f8bcc76b38f43074ed1a1b0d35f79c8a9f
Lozumi Everkine <fantasynico1026@gmail.com>
2023-10-07 20:50:33 +0800

    Complete the activity system

--- src/main/java/com.lozumi.NAMS/Activity.java
+++ src/main/java/com.lozumi.NAMS/Activity.java
@@ -1,16 +1,33 @@
 package com.lozumi.NAMS;
 
-import javax.swing.text.html.HTMLDocument;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 import java.util.Objects;
 
 public class Activity {
-    String id;
-    String name;
-    Date startTime;
-    Date endTime;
-    String description;
-    String site;
+    private String id;
+    private String name;
+    private Date startTime;
+    private Date endTime;
+    private String description;
+    private String site;
+    //创建列表
+    private List<Team> teams;
+    private List<Teacher> teachers;
+    private List<Organizer> organizers;
+
+    public Activity(String id, String name, Date startTime, Date endTime, String description, String site) {
+        this.id = id;
+        this.name = name;
+        this.startTime = startTime;
+        this.endTime = endTime;
+        this.description = description;
+        this.site = site;
+        this.teams = new ArrayList<>();
+        this.teachers = new ArrayList<>();
+        this.organizers = new ArrayList<>();
+    }
 
     public String getId() {
         return this.id;
@@ -40,23 +57,55 @@ public class Activity {
         this.description = description;
     }
 
-    public void addTeamToActivity(Team team){
-
+    public void addTeamToActivity(Team team) {
+        teams.add(team);
     }
-    public void removeTeamFromActivity(String id){
 
+    public void removeTeamFromActivity(String id) {
+        for (Team team : teams) {
+            if (team.getTeamId().equals(id)) {
+                teams.remove(team);
+                break;
+            }
+        }
     }
-    public void addTeacherToActivity(Teacher teacher){
 
+    public void addTeacherToActivity(Teacher teacher) {
+        teachers.add(teacher);
     }
-    public void removeTeacherFromActivity(String id){
 
+    public void removeTeacherFromActivity(String id) {
+        for (Teacher teacher : teachers) {
+            if (teacher.getId().equals(id)) {
+                teachers.remove(teacher);
+                break;
+            }
+        }
     }
-    public void addOrganizerToActivity(Organizer organizer){
 
+    public void addOrganizerToActivity(Organizer organizer) {
+        organizers.add(organizer);
     }
-    public void removeOrganizerFromActivity(String id){
 
+    public void removeOrganizerFromActivity(String id) {
+        for (Organizer organizer : organizers) {
+            if (organizer.getId().equals(id)) {
+                organizers.remove(organizer);
+                break;
+            }
+        }
+    }
+
+    public List<Team> getTeamsIterator() {
+        return teams;
+    }
+
+    public List<Teacher> getTeachersIterator() {
+        return teachers;
+    }
+
+    public List<Organizer> getOrganizersIterator() {
+        return organizers;
     }
 
     @Override
@@ -72,33 +121,16 @@ public class Activity {
         return Objects.hash(id);
     }
 
-    public Iterator getTeamsIterator(){
-
-    }
-    public Iterator getTeachersIterator(){
-
-    }
-    public Iterator getOrganizersIterator(){
-
-    }
-
     @Override
     public String toString() {
         final StringBuilder sb = new StringBuilder("Activity").append('[')
-                .append("id=")
-                .append(id)
-                .append("_name=")
-                .append(name)
-                .append("_startTime=")
-                .append(startTime)
-                .append("_endTime=")
-                .append(endTime)
-                .append("_description=")
-                .append(description)
-                .append("_site=")
-                .append(site)
+                .append("id=").append(id)
+                .append(", name=").append(name)
+                .append(", startTime=").append(startTime)
+                .append(", endTime=").append(endTime)
+                .append(", description=").append(description)
+                .append(", site=").append(site)
                 .append(']');
         return sb.toString();
     }
 }
-
--- src/main/java/com.lozumi.NAMS/ActivitySubSystem.java
+++ src/main/java/com.lozumi.NAMS/ActivitySubSystem.java
@@ -8,19 +8,56 @@ public class ActivitySubSystem {
     public void addActivity(Activity activity){
         aList.add(activity);
     }
+
     public void removeActivityById(String id){
-
+        // 根据活动ID删除活动
+        Activity activityToRemove = null;
+        for (Activity activity : aList) {
+            if (activity.getId().equals(id)) {
+                activityToRemove = activity;
+                break;
+            }
+        }
+        if (activityToRemove != null) {
+            aList.remove(activityToRemove);
+        }
     }
+
     public Activity getActivityById(String id){
-
+        // 根据活动ID查找活动并返回
+        for (Activity activity : aList) {
+            if (activity.getId().equals(id)) {
+                return activity;
+            }
+        }
+        return null; // 如果找不到活动，则返回null
     }
+
     public Activity getActivityByDate(Date date){
-
+        // 根据日期查找活动并返回
+        for (Activity activity : aList) {
+            if (activity.getStartTime().compareTo(date) <= 0 && activity.getEndTime().compareTo(date) >= 0) {
+                return activity;
+            }
+        }
+        return null; // 如果找不到活动，则返回null
     }
+
     public List<Activity> getActivityList(){
-
+        // 返回所有活动列表
+        return aList;
     }
-    public int checkActivityNumOfTeam(String teamId){
 
+    public int checkActivityNumOfTeam(String teamId){
+        // 计算特定团队参与的活动数量
+        int count = 0;
+        for (Activity activity : aList) {
+            for (Team team : activity.getTeamsIterator()) {
+                if (team.getTeamId().equals(teamId)) {
+                    count++;
+                }
+            }
+        }
+        return count;
     }
 }
